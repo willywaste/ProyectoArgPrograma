f2df8b8fe9ce59895189dd18bb73d902f1a4ff21
willywaste <guillermovillalba2@gmail.com>
2022-09-28 22:30:50 -0300

    ultimo septiembre

--- ProyectoArgProg/FrontEnd/src/app/app.module.ts
+++ ProyectoArgProg/FrontEnd/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { FooterComponent } from './componentes/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 
 
@@ -36,15 +38,17 @@ import { LoginComponent } from './componentes/login/login.component';
     ProyectosComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [interceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ProyectoArgProg/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoArgProg/FrontEnd/src/app/service/persona.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { persona } from '../model/persona.model';
 
+
+
+
 @Injectable({
   providedIn: 'root'
 })
